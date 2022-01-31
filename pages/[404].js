eebd40b81175c6050cd48d97e9c010d49b43b439
Mohit Yadav <www.mohit2004@gmail.com>
2022-01-31 21:30:24 +0530

    updated

--- pages/[404].js
+++ pages/[404].js
@@ -1,13 +1,12 @@
 import React from "react";
 import NotFound from "../components/NotFound/NotFound";
-import styles from "../styles/notFoundPage.component.scss";
+import styles from "../styles/notFoundPage.module.scss";
 
 const NotFoundPage = () => {
   // Amazing 404 page
   return (
     <div className={styles.container}>
-      {/* <Page404 /> */}
-      Not Found
+      <NotFound />
     </div>
   );
 };
