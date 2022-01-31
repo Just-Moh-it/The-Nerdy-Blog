3a020cddf6c0a9dfa0d06a1eaea0b80dc10ded3b
Mohit Yadav <www.mohit2004@gmail.com>
2022-01-31 20:26:39 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
-    sassOptions: {
-      includePaths: [path.join(__dirname, 'styles')],
-      prependData: `@import "~@styles/variables.scss";`
-    }
+    // sassOptions: {
+    //   includePaths: [path.join(__dirname, 'styles')],
+    //   prependData: `@import "~@styles/variables.scss";`
+    // }
 }
